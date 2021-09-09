1a7c947dd787e1f4135f62c4b58d4737729977bd
meghrxj <hello.meghrajjj@gmail.com>
2021-09-09 23:11:49 +0530

    added poolpuzzle

---
+++ PoolPuzzle/PP1/EchoV1.java
@@ -0,0 +1,15 @@
+package PoolPuzzle.PP1;
+
+public class EchoV1 {
+
+    int count = 0;
+
+    void hello() {
+
+        System.out.println("Helloooooooooo...");
+    }
+
+    public EchoV1 getEchoV1() {
+        return null;
+    }
+}
---
+++ PoolPuzzle/PP1/EchoV1TestDrive.java
@@ -0,0 +1,31 @@
+package PoolPuzzle.PP1;
+
+public class EchoV1TestDrive {
+    
+    public static void main(String[] args) {
+
+        EchoV1 e1 = new EchoV1();
+        
+        EchoV1 e2 = e1.getEchoV1();
+
+        int x = 0;
+
+        while ( x < 4 ) {
+            e1.hello();
+            e1.count = e1.count + 1;
+
+            if ( x == 3 ) {
+
+                e2.count = e2.count + 1;
+            }
+            
+            if ( x > 0 ) {
+                e2.count = e2.count + e1.count;
+            }
+
+            x = x + 1;
+
+        }
+        System.out.println(e2.count);
+    }
+}
