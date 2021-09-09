141b7e9673b1f821d0d08b35daf464fadc6c9a0c
meghrxj <hello.meghrajjj@gmail.com>
2021-09-09 23:03:18 +0530

    added CM

---
+++ Codemagnets/CM1/DrumKit.java
@@ -0,0 +1,14 @@
+package Codemagnets.CM1;
+
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+}
---
+++ Codemagnets/CM1/DrumKitTestDrive.java
@@ -0,0 +1,16 @@
+package Codemagnets.CM1;
+
+public class DrumKitTestDrive {
+
+    public static void main(String [] args) {
+
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        d.playTopHat();
+        
+        if (d.snare == true) {
+            d.playSnare();
+        }
+    }
+}
