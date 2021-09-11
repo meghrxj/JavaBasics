a24b58b7cb4f350461948a95262f892c59f2d796
meghrxj <hello.meghrajjj@gmail.com>
2021-09-11 23:46:28 +0530

    some shit

---
+++ Other/MyProfile.java
@@ -0,0 +1,13 @@
+package Other;
+
+public class MyProfile {
+	public static void main(String[] args) {   
+
+
+      String name = "Meg";
+      int age = 17;
+      double desiredCGPA = 8.9;
+      char gender = 'm';
+      boolean lookingForJob = false;
+	}
+}
