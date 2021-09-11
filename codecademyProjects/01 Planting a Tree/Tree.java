8b14a8cdbf7a3f6257441b84279c6c1dbcdd4762
meghrxj <hello.meghrajjj@gmail.com>
2021-09-12 00:01:29 +0530

    added some shit

---
+++ codecademyProjects/01 Planting a Tree/Tree.java
@@ -0,0 +1,18 @@
+package codecademyProjects;
+
+public class Tree {
+    
+    public static void main(String[] args) {
+    // We’re going to introduce ourselves and print a tree to the screen.
+        System.out.println("Hey, my name is Meghraj.");
+        System.out.println("I am learning to plant a new tree on a terminal");
+
+
+        System.out.println("  *  ");
+	    System.out.println(" *** ");
+	    System.out.println(" *** ");
+	    System.out.println("  *  ");
+	    System.out.println("  *  ");  
+    }
+    
+}
